e19f3f94b6dae0cab19df0af0618d7f3a13d2be9
uniquening <34419369@qq.com>
2019-05-08 23:27:28 +0800

    轮播图制作完毕，学习歌曲数据接口

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <m-tab></m-tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import MTab from 'components/tab/tab'
+export default {
+  name: 'App',
+  components: {
+    MHeader,
+    MTab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color: $color-theme
+</style>
---
+++ main.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+// Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+/* eslint-disable no-new */
+fastclick.attach(document.body)
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
